212699de64a6bf27dcd6311b5ebb330946707220
Amandeep Grewal <agrewal@twitter.com>
2013-07-27 15:27:01 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blingpass::Application.config.secret_key_base = '83ea0123cc8cabef18b61c438d441a3924ce28b982da330b43145bc3e25d78f92ef3b367811280d8815b4a0a722e9cf9e99432762631e5463fdf62d0ac6a5f61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blingpass::Application.config.session_store :cookie_store, key: '_blingpass_session'
