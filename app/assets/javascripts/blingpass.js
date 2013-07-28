125e03542283c9cb622d9064d27d077ef10766e1
John Liu <johnyuhanliu@gmail.com>
2013-07-28 01:32:07 -0700

    Updates blingpass.js to pre-alpha version 5.

--- app/assets/javascripts/blingpass.js
+++ app/assets/javascripts/blingpass.js
@@ -4,9 +4,6 @@ $(function() {
   var $forms = $('form.blingpass-form');
   var $passFields = $('form.blingpass-form input.blingpass-password');
   var passModels = {};
-  var keyMap = {};
-  var lastHold = 0;
-  var lastFlight = 0;
 
   var validKeyCode = function(keyCode, shiftKey, ctrlKey, altKey, metaKey) {
     var exclude = [91, 92, 93, 37, 38, 39, 40, 45, 46, 36, 35, 33, 34];
@@ -20,48 +17,40 @@ $(function() {
     }
 
     passModels[$this.attr('id')] = {
-      holds: [],
-      flights: [0]
+      downStamps: [],
+      upStamps: [],
     }
   });
 
-  $passFields.bind('keydown', function(event) {
-    console.log(event.keyCode);
-    if (validKeyCode(event.keyCode, event.shiftKey, event.ctrlKey, event.altKey, event.metaKey)) {
-      var $this = $(this);
-      var $obj = passModels[$this.attr('id')];
-      lastHold = new Date();
-      keyMap[event.keyCode] = keyMap[event.keyCode] + 1 || 1;
-
-      if (lastFlight) {
-        $obj.flights.push((new Date()) - lastFlight);
-      }
-
-      console.log('flights: ' + $obj.flights);
-      console.log('count flight: ' + $obj.flights.length);
-    }
-  });
-
-  $passFields.bind('keyup', function(event) {
+  $passFields.keydown(function(event) {
     var $this = $(this);
-    if (keyMap[event.keyCode] && keyMap[event.keyCode] > 0) {
-      keyMap[event.keyCode]--;
+    var $obj = passModels[$this.attr('id')];
+    var keyCode = event.keyCode;
 
-      var $obj = passModels[$this.attr('id')];
-      $obj.holds.push((new Date()) - lastHold);
-      lastFlight = new Date();
+    if (validKeyCode(keyCode, event.shiftKey, event.ctrlKey, event.altKey, event.metaKey)) {
+      $obj.downStamps.push({
+        keyCode: keyCode,
+        amount: new Date().getTime()
+      });
+    }
+  });
 
-      console.log('holds: ' + $obj.holds);
-      console.log('count holds: ' + $obj.holds.length);
+  $passFields.keyup(function(event) {
+    var $this = $(this);
+    var $obj = passModels[$this.attr('id')];
+    var keyCode = event.keyCode;
+
+    if (validKeyCode(keyCode, event.shiftKey, event.ctrlKey, event.altKey, event.metaKey)) {
+      $obj.upStamps.push({
+        keyCode: keyCode,
+        amount: new Date().getTime()
+      });
     }
 
     if ($this.val().length === 0) {
-      keyMap = {};
-      lastHold = 0;
-      lastFlight = 0;
       passModels[$this.attr('id')] = {
-        holds: [],
-        flights: [0]
+        downStamps: [],
+        upStamps: []
       }
     }
   });
@@ -74,7 +63,25 @@ $(function() {
       json[$each.attr('name')] = $each.val();
 
       if ($each.hasClass('blingpass-password')) {
-        $.extend(json, passModels[$each.attr('id')]);
+        var $obj = passModels[$each.attr('id')];
+        var extension = {};
+
+        json.flights = [0];
+        $obj.downStamps.reduce(function(previous, current, index, array) {
+          extension.flights.push(current.amount - previous.amount);
+          return current;
+        });
+
+        json.holds = [];
+        $obj.downStamps.forEach(function(val) {
+          for (var i = 0; i < $obj.upStamps.length; i++) {
+            if (val.keyCode === $obj.upStamps[i].keyCode) {
+              extension.holds.push($obj.upStamps[i].amount - val.amount);
+              $obj.upStamps.splice(i, 1);
+              break;
+            }
+          }
+        });
       }
     });
 
@@ -87,8 +94,11 @@ $(function() {
         $this.children('.error').text(response.status).addClass('showing');
       }
 
-      console.log(response);
+      window.setTimeout(function() {
+        $this.children('.error').removeClass('showing');
+      }, 500);
     });
+
     return false;
   });
 });
