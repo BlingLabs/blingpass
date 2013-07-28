12c15eddb74330aba865a217e4ec2f4b86066d21
Freddy Chen <freddykychen@gmail.com>
2013-07-27 20:20:38 -0700

    update verifier to adjust threshold

--- app/utils/verifier.rb
+++ app/utils/verifier.rb
@@ -1,20 +1,34 @@
 class Verifier
-	THRESHOLD = 0.1
-	def self.verify(user, hold, flight)
-		for i in 0..(hold.length - 1)
+	MAX_THRESHOLD = 0.5
+	MIN_THRESHOLD = 0.3
+	
+	def self.verify(user, new_holds, new_flights)
+		accuracy_count = 0
+		for i in 0..(new_holds.length - 1)
 			# Calculate deviation between new hold/flight time with avg hold/flight time
-			deviation = Math::sqrt((hold[i] - user.avg_hold[i])**2 + (flight[i] - user.avg_flight[i])**2)
+			deviation = Math::sqrt((new_holds[i] - user.holds[i])**2 + (new_flights[i] - user.flights[i])**2)
 
-			# If the deviation is too high just bail out right away
-			if deviation > THRESHOLD
+			if deviation > user.threshold
+				# If the deviation is too high just bail out right away
 				return false
+			elsif deviation < MIN_THRESHOLD
+				# If the deviation is really small, increase the accuracy count
+				accuracy_count += 1
 			end
-
+				
 			# Update averages and count so that we get more accurate over time
-			user.avg_hold[i] = (user.avg_hold[i] * user.count + hold[i]) / (user.count + 1)
-			user.avg_flight[i] = (user.avg_flight[i] * user.count + flight[i]) / (user.count + 1)
-			user.count += 1
+			user.holds[i] = (user.holds[i] * user.count + new_holds[i]) / (user.count + 1)
+			user.flights[i] = (user.flights[i] * user.count + new_flights[i]) / (user.count + 1)
 		end
+		user.count += 1
+
+		# Decrease threshold if the user is really accurate
+		if accuracy_count == new_holds.length
+			user.threshold -= 0.01 if user.threshold > MIN_THRESHOLD
+		else
+			user.threshold += 0.01 if user.threshold < MAX_THRESHOLD
+		end
+
 		return true
 	end
 end
