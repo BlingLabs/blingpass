90905b49b39be14517171dfba66c7774a71595ac
Amandeep Grewal <me@amandeep.ca>
2013-07-27 22:40:27 -0700

    Update count of user in training period

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,8 +10,8 @@ class Api::UsersController < ApplicationController
     u = User.new
     u.username = get_combined_username
     u.password = params[:user][:username]
-    u.holds = params[:user][:holds].map { |i| i.to_s.to_i }
-    u.flights = params[:user][:flights].map { |i| i.to_s.to_i }
+    u.holds = to_int_array params[:user][:holds]
+    u.flights = to_int_array params[:user][:flights]
 
     ret = u.save!
 
@@ -25,11 +25,15 @@ class Api::UsersController < ApplicationController
     end
 
     u = all_users.first
-    ret = authenticate u, user_params
 
     if u.count < 5
-      render json: { status: :need_more_logins.to_s + u.count.to_s } and return
+      Verifier.update_average(u, to_int_array(user_params[:holds]), to_int_array(user_params[:flights]))
+      u.count = u.count + 1
+      u.save!
+      render json: { status: :need_more_logins } and return
     end
+
+    ret = authenticate u, user_params
 
     render json: { status: (ret ? :sucessful_verification : :failed_verification) }
   end
@@ -41,13 +45,17 @@ class Api::UsersController < ApplicationController
   end
 
   def authenticate(u, u_candidate)
-    ret = Verifier.verify(u, u_candidate[:holds].map { |i| i.to_s.to_i }, u_candidate[:flights].map { |i| i.to_s.to_i })
+    ret = Verifier.verify(u, to_int_array(u_candidate[:holds]), to_int_array(u_candidate[:flights]))
     u.save!
     ret
   end
 
   def get_combined_username
     (params[:service] ? params[:service] : 'none') + ':' + params[:user][:username]
+  end
+
+  def to_int_array(arr)
+    arr.map { |i| i.to_s.to_i }
   end
 
 end
