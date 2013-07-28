187de776bfb955db83bc313fc54532ddc57b7289
John Liu <johnyuhanliu@gmail.com>
2013-07-28 07:31:24 -0700

    Fixes timing issue.

--- app/assets/javascripts/blingpass.js
+++ app/assets/javascripts/blingpass.js
@@ -57,52 +57,54 @@ $(function() {
 
   $forms.submit(function() {
     var $this = $(this);
-    var json = {};
-    $this.children('input').each(function() {
-      $each = $(this);
-      json[$each.attr('name')] = $each.val();
+    window.setTimeout(function() {
+      var json = {};
+      $this.children('input').each(function() {
+        $each = $(this);
+        json[$each.attr('name')] = $each.val();
 
-      if ($each.hasClass('blingpass-password')) {
-        var $obj = passModels[$each.attr('id')];
+        if ($each.hasClass('blingpass-password')) {
+          var $obj = passModels[$each.attr('id')];
 
-        json.flights = [];
-        if ($obj.downStamps.length > 0) {
-          json.flights.push(0);
-          $obj.downStamps.reduce(function(previous, current, index, array) {
-            json.flights.push(current.amount - previous.amount);
-            return current;
+          json.flights = [];
+          if ($obj.downStamps.length > 0) {
+            json.flights.push(0);
+            $obj.downStamps.reduce(function(previous, current, index, array) {
+              json.flights.push(current.amount - previous.amount);
+              return current;
+            });
+          }
+
+          json.holds = [];
+          var upStamps = $.extend(true, {}, { array: $obj.upStamps });
+          upStamps = upStamps.array;
+          $obj.downStamps.forEach(function(val) {
+            for (var i = 0; i < upStamps.length; i++) {
+              if (val.keyCode === upStamps[i].keyCode) {
+                json.holds.push(upStamps[i].amount - val.amount);
+                upStamps.splice(i, 1);
+                break;
+              }
+            }
           });
         }
+      });
 
-        json.holds = [];
-        var upStamps = $.extend(true, {}, { array: $obj.upStamps });
-        upStamps = upStamps.array;
-        $obj.downStamps.forEach(function(val) {
-          for (var i = 0; i < upStamps.length; i++) {
-            if (val.keyCode === upStamps[i].keyCode) {
-              json.holds.push(upStamps[i].amount - val.amount);
-              upStamps.splice(i, 1);
-              break;
-            }
-          }
-        });
-      }
-    });
+      console.log(json);
+      $.post($this.attr('action'), { "user": json }, function(response) {
+        if ($this.attr('id').indexOf('register') >= 0) {
+          console.log($this.children('.error'));
+          $this.children('.error').text(response.status).addClass('showing');
+        } else if ($this.attr('id').indexOf('login') >= 0) {
+          $this.children('.error').text(response.status).addClass('showing');
+        }
 
-    console.log(json);
-    $.post($this.attr('action'), { "user": json }, function(response) {
-      if ($this.attr('id').indexOf('register') >= 0) {
-        console.log($this.children('.error'));
-        $this.children('.error').text(response.status).addClass('showing');
-      } else if ($this.attr('id').indexOf('login') >= 0) {
-        $this.children('.error').text(response.status).addClass('showing');
-      }
+        window.setTimeout(function() {
+          $this.children('.error').removeClass('showing');
+        }, 500);
+      });
 
-      window.setTimeout(function() {
-        $this.children('.error').removeClass('showing');
-      }, 500);
-    });
-
+    }, 100);
     return false;
   });
 });
